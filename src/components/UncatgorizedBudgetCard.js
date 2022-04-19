287baf94430651594d0af56ab8463102820c81cd
travis flake <tflake83@gmail.com>
2022-04-19 17:59:37 -0400

    typo cahnge

--- src/components/UncatgorizedBudgetCard.js
+++ src/components/UncatgorizedBudgetCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { UNCATAGORIZED_BUDGET_ID, useBudgets } from '../contexts/BudgetContexts'
+import { UNCATEGORIZED_BUDGET_ID, useBudgets } from '../contexts/BudgetContexts'
 import BudgetCard from './BudgetCard'
 
 export default function UncatgorizedBudgetCard(props) {
     const { getBudgetExpenses } = useBudgets()
-    const amount = getBudgetExpenses(UNCATAGORIZED_BUDGET_ID).reduce((total, expense) => total + expense.amount, 0)
+    const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce((total, expense) => total + expense.amount, 0)
     if (amount === 0) return null;
 
   return (
-    <BudgetCard amount={amount} name='Uncatgorized' gray {...props} />
+    <BudgetCard amount={amount} name='Uncategorized' gray {...props} />
   )
 }
